07868f72237df8e4788ceecdb7b3cb9d204b59cd
TEO OLIVER HUGHES <teo_hughes@icloud.com>
2021-03-11 18:53:10 +0000

    Initial Commit

---
+++ Circuit Simulator/Circuit_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Circuit_SimulatorApp.swift
+//  Circuit Simulator
+//
+//  Created by Hughes, Teo (BJH) on 11/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Circuit_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
